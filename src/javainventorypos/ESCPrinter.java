14a2f6127be54a9c762c6f5580b3ef7a2f2ecceb
Zion Chan <37270236+zionericc@users.noreply.github.com>
2019-09-23 15:43:57 +0800

    Update ESCPrinter.java

--- src/javainventorypos/ESCPrinter.java
+++ src/javainventorypos/ESCPrinter.java
@@ -1,8 +1,43 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 
+Copyright (c) 2006-2007, Giovanni Martina
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that 
+the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, this list of conditions and the 
+following disclaimer.
+
+- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
+following disclaimer in the documentation and/or other materials provided with the distribution.
+
+-Neither the name of Drayah, Giovanni Martina nor the names of its contributors may be used to endorse or 
+promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY 
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+DAMAGE.
+
+ * 
+ * ESCPrinter.java
+ *
+ * Created on 10 de Setembro de 2006, 13:57
+ *
+ * @author Giovanni <gio@drayah.net>
+ * Copyright © 2006 G.M. Martina
+ *
+ * Class that enables printing to ESC/P and ESC/P2 dot matrix printers (e.g. Epson LQ-570, Epson LX-300) by writing directly to a stream using standard I/O
+ * Like this we have direct control over the printer and bypass Java2D printing which is considerably slower printing in graphics to a dotmatrix
+ *
  */
+
 package javainventorypos;
 
 /**
